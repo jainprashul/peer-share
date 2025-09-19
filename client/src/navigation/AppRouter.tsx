703c8fea4b1ebc327b89e522340de8489fbd2e8a
Rohit <rohithattimare1997@gmail.com>
2025-09-19 14:22:35 +0530

    Refactor AppRouter to conditionally render layout based on route, improving routing logic and organization., no Header Layout in call page

--- client/src/navigation/AppRouter.tsx
+++ client/src/navigation/AppRouter.tsx
@@ -1,7 +1,7 @@
 import Layout from './Layout'
-import { BrowserRouter, Route, Routes, useLocation, useNavigate, useParams} from 'react-router'
+import { BrowserRouter, Route, Routes, useLocation, useNavigate, useParams } from 'react-router'
 import Home from '../pages/home'
-import Group from '../pages/group'  
+import Group from '../pages/group'
 import Call from '../pages/call'
 import Fallback from '../components/Fallback'
 import { history } from './history'
@@ -14,11 +14,11 @@ const AppRouter = () => {
     }, []);
 
     return (
-        <Layout>
-            <BrowserRouter>
-                <AppRouterContent />
-            </BrowserRouter>
-        </Layout>
+        //<Layout>
+        <BrowserRouter>
+            <AppRouterContent />
+        </BrowserRouter>
+        //</Layout>
     )
 }
 
@@ -28,13 +28,29 @@ function AppRouterContent() {
     history.navigate = useNavigate();
     history.location = useLocation();
     history.params = useParams();
+
+    // Routes where you don't want layout
+    const noLayoutRoutes = ["/call"];
+
+    const isNoLayout = noLayoutRoutes.includes(location.pathname);
+
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/group" element={<Group />} />
-            <Route path="/group/:groupId" element={<Group />} />
-            <Route path="/call" element={<Call />} />
-            <Route path="*" element={<Fallback />} />
-        </Routes>
-    )
+        <>
+            {isNoLayout ? (
+                <Routes>
+                    <Route path="/call" element={<Call />} />
+                </Routes>
+            ) : (
+                <Layout>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/group" element={<Group />} />
+                        <Route path="/group/:groupId" element={<Group />} />
+                        <Route path="*" element={<Fallback />} />
+                    </Routes>
+                </Layout>
+            )}
+        </>
+    );
+
 }
