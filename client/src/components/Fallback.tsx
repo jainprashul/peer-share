a4bdc183e4dd65fac34abb235ee38d744c70eedd
Rohit <rohithattimare1997@gmail.com>
2025-09-19 13:50:09 +0530

    Add custom 404 page with styling and background image

--- client/src/components/Fallback.tsx
+++ client/src/components/Fallback.tsx
@@ -1,17 +1,28 @@
 import { Link } from 'react-router'
+import './Fallback.css'
 
 function Fallback() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen text-center">
-            <h1 className="text-6xl font-bold text-red-500">404</h1>
-            <p className="text-xl mt-4">Oops! The page you’re looking for doesn’t exist.</p>
-            <Link
-                to="/"
-                className="mt-6 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
-            >
-                Go Home
-            </Link>
-        </div>
+        <section className="page_404 h-full mx-auto">
+            <div className="container  px-4">
+                <div className="flex justify-center items-center min-h-screen">
+                    <div className="text-center w-full max-w-3xl">
+
+                        <div className="four_zero_four_bg flex items-center justify-center ">
+                            <h1 className='txt_404'>404</h1>
+                        </div>
+                        <div>
+                            <h3>Look like you're lost</h3>
+                            <p>The page you are looking for is not available!</p>
+
+                            <Link to="/" className="link_404">
+                                Go Home
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
